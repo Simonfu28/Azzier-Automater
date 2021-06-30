fd65c025c287a27a0eca78f54d0d2c2aaa6ddb16
simon.fu <simon.fu@avcorp.com>
2021-06-30 09:18:32 -0700

    Working on UI stuff

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
+import sys
+
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import PyQt5
-from PyQt5 import QtWidgets, uic, QtGui
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
 
 from configparser import ConfigParser
 
 config = ConfigParser()
 config.read('config.cfg')
 
-# opens webpage
-driver = selenium.webdriver.Chrome(config.get("Azzier", 'chrome_webdriver'))
-driver.get(config.get("Azzier", "pm_URL"))
+global driver
+
+
+class Main(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(Main, self).__init__()   # call inherited class
+        uic.loadUi('main.ui', self)    # Loads .ui file
+
+        buttonBox = QtWidgets.QDialogButtonBox()
+        buttonBox.accepted().connect(self.accept)
+
+        self.show()
+
+    def accept(self):
+        print("s")
 
 
 def login(username, password):
+    global driver
+    # opens webpage
+    driver = selenium.webdriver.Chrome(config.get("Azzier", 'chrome_webdriver'))
+    driver.get(config.get("Azzier", "pm_URL"))
+
     # closes timeout popup
     timeout_obj = driver.switch_to.alert
     timeout_obj.accept()
@@ -42,8 +61,10 @@ def change_WOgeneration(bool):
 
 
 def main():
-    login(config.get("Azzier", 'username'), config.get('Azzier', 'password'))
-    search_pm('512')
+    #login('SFU', 'Maint123')
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main()
+    app.exec_()
 
 
 if __name__ == '__main__':
